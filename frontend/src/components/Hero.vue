f86b4e823208893211c1c65ee4cec4b89a2029d7
DI2T28 <diditgunawan711@gmail.com>
2025-07-10 15:01:50 +0700

    tes

--- frontend/src/components/Hero.vue
+++ frontend/src/components/Hero.vue
@@ -3,7 +3,7 @@
 <div class="md:w-1/2 mb-10 md:mb-0">
 <h1 class="text-5xl font-bold text-gray-800 mb-4 animate-slide-in-left" style="animation-delay:
 200ms;">
-Halo, Saya <span class="text-blue-600">Ferdy</span>
+Halo, Saya <span class="text-blue-600">Didit</span>
 </h1>
 <p class="text-xl text-gray-600 mb-8 animate-slide-in-left" style="animation-delay: 400ms;">
 Mahasiswa Teknik Informatika yang bersemangat dalam pengembangan web dan desain
